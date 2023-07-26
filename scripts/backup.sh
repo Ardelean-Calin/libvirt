a3d72b477c93108fcb88d75ffbc7b5f3457d56b9
Ardelean Calin <9417983+Ardelean-Calin@users.noreply.github.com>
2023-07-26 11:09:30 +0300

    Added notifications.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+show_notification ()
+{
+  gdbus call --session \
+    --dest=org.freedesktop.Notifications \
+    --object-path=/org/freedesktop/Notifications \
+    --method=org.freedesktop.Notifications.Notify \
+    "" 0 "" "$1" "$2" \
+    '[]' '{"urgency": <1>}' 5000
+}
+
 echo "Starting backup of virtual machines..."
+show_notification "VM Backup ongoing" "Started backup of virtual machine images."
 rsync $HOME/.libvirt/images/ -r --delete dietpi@10.134.6.112:/mnt/hdd/data/VMs
+show_notification "VM Backup finised!" "Finished backup of all virtual machine images."
 echo "Backup done."
